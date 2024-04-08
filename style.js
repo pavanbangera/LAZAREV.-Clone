94347e3e6bf356133d2db919521c6b25307ecf86
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2024-04-08 16:28:54 +0530

    page7 done

--- style.js
+++ style.js
@@ -1,3 +1,40 @@
+function locoScroll () {
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop (value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect () {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+        pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+
+
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+
+}
+
+
+
+
+
 function navAnim () {
     let nav = document.querySelector(".nav-part2");
 
@@ -46,19 +83,19 @@ function page2Anim () {
     let elems = document.querySelectorAll(".p2-elem");
 
     elems.forEach(elem => {
-        elem.addEventListener("mouseenter", () => {
+        elem.lastElementChild.addEventListener("mouseenter", () => {
             gsap.to(elem.childNodes[5], {
                 opacity: 1,
                 scale: 1
             })
         })
-        elem.addEventListener("mouseleave", () => {
+        elem.lastElementChild.addEventListener("mouseleave", () => {
             gsap.to(elem.childNodes[5], {
                 opacity: 0,
                 scale: 0
             })
         })
-        elem.addEventListener("mousemove", (px) => {
+        elem.lastElementChild.addEventListener("mousemove", (px) => {
             gsap.to(elem.childNodes[5], {
                 x: px.x - elem.getBoundingClientRect().x - 50,
                 y: px.y - elem.getBoundingClientRect().y - 50
@@ -92,17 +129,33 @@ function page3Anim () {
 function page7Anim () {
     let containers = document.querySelectorAll(".p7-right");
     containers.forEach((container) => {
-        container.addEventListener("mouseenter", () => {
+        container.children[2].addEventListener("mouseenter", () => {
+            gsap.to(container.children[1], {
+                opacity: 1,
+                scale: 1
+            })
             container.childNodes[1].play();
-        })
-        container.addEventListener("mouseleave", () => {
+
+        }, 'p7')
+        container.children[2].addEventListener("mouseleave", () => {
+            gsap.to(container.children[1], {
+                opacity: 0,
+                scale: 0
+            })
             container.childNodes[1].load();
-        })
+        }, 'p7')
+        container.children[2].addEventListener("mousemove", (px) => {
+            gsap.to(container.children[1], {
+                x: px.x - container.getBoundingClientRect().x - 400,
+                y: px.y - container.getBoundingClientRect().y - 100
+            })
+        }, 'p7')
     })
 }
 
 
-navAnim();
-page2Anim();
-page3Anim();
-page7Anim();
+// locoScroll();
+// navAnim();
+// page2Anim();
+// page3Anim();
+// page7Anim();
