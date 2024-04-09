50e7b594554f76320ec35568ae36370a4a6d94f5
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2024-04-09 14:48:06 +0530

    page8 done

--- style.js
+++ style.js
@@ -1,33 +1,10 @@
 function locoScroll () {
-    gsap.registerPlugin(ScrollTrigger);
 
-    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
     const locoScroll = new LocomotiveScroll({
         el: document.querySelector("#main"),
         smooth: true
     });
-    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-    locoScroll.on("scroll", ScrollTrigger.update);
-
-    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-    ScrollTrigger.scrollerProxy("#main", {
-        scrollTop (value) {
-            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-        getBoundingClientRect () {
-            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-        },
-        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-        pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-    });
-
-
-    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-    ScrollTrigger.refresh();
 
 }
 
@@ -66,18 +43,41 @@ function navAnim () {
             stagger: {
                 amount: 0.2
             }
-        }, 'b')
+        }, 'a')
         tl.to(".nav-part2 h5", {
             display: "none",
             duration: 0.1
-        }, 'b')
+        }, 'a')
         tl.to("#nav-bottom", {
             height: 0,
             duration: 0.1
-        }, 'b')
+        }, 'a')
     })
 };
 
+function page1Anim () {
+    let t1 = gsap.timeline();
+
+    t1.from("#page1", {
+        opacity: 0
+    })
+    t1.from("#page1", {
+        transform: "scaleX(0.6) scaleY(0.2)",
+        borderRadius: "50px",
+        top: "100",
+        duration: "2"
+    })
+    t1.from("#page1 h1,#page1 p,#page1 div", {
+        transform: "scaleX(0.9) scaleY(0.7)",
+        stagger: {
+            amount: 0.5
+        }
+    })
+    t1.from("nav", {
+        opacity: 0,
+        duration: 1
+    })
+}
 
 function page2Anim () {
     let elems = document.querySelectorAll(".p2-elem");
@@ -152,10 +152,49 @@ function page7Anim () {
         }, 'p7')
     })
 }
+function page8Anim () {
 
+
+    let boxes = document.querySelectorAll(".p8-box");
+    boxes.forEach(box => {
+        box.addEventListener("mouseenter", () => {
+            let t1 = gsap.timeline();
+            t1.to(box.children[1], {
+                display: "none"
+
+            }, "p8")
+            t1.to(box.children[2], {
+                display: "none"
+            }, "p8")
+            t1.to(box.children[3], {
+                height: "85%",
+                duration: 1
+
+            }, "p8")
+        })
+        box.addEventListener("mouseleave", () => {
+            let t1 = gsap.timeline();
+            t1.to(box.children[1], {
+                display: "block"
+
+            }, "p8")
+            t1.to(box.children[2], {
+                display: "block"
+            }, "p8")
+            t1.to(box.children[3], {
+                height: "50%",
+                duration: 1
+
+            }, "p8")
+        })
+    })
+}
 
 // locoScroll();
+// page1Anim();
+
 // navAnim();
 // page2Anim();
 // page3Anim();
 // page7Anim();
+// page8Anim();
